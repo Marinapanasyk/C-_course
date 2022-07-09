7d12ab85969c746ddcb2c5a0a07cefffecc95469
Marinapanasyk <marinapanasyk72@mail.ru>
2022-07-09 21:56:42 +0300

    задание_1_2

--- Homework/Lesson_1/1.2/Program.cs
+++ Homework/Lesson_1/1.2/Program.cs
@@ -1,15 +1,15 @@
 ﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 
-Console.WriteLine ("Write a numberа:");
+Console.WriteLine("Write a number a: ");
 int a = int.Parse(Console.ReadLine());
 
-Console.WriteLine ("Write b numberb:");
+Console.WriteLine("Write b number b: ");
 int b = int.Parse(Console.ReadLine());
 
 Console.Write("max=");
-if a > b;
+if (a > b)
 Console.WriteLine(a);
 else
-{Console.WriteLine(b);
+Console.WriteLine(b);
     
-}
+
