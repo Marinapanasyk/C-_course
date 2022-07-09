2c6885460ddfe64127e677795c12fad5126ac497
Marinapanasyk <marinapanasyk72@mail.ru>
2022-07-09 22:47:31 +0300

    задание_1_3

--- Homework/Lesson_1/1.3/Program.cs
+++ Homework/Lesson_1/1.3/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.WriteLine("Write a number a: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Write a number b: ");
+int b = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Write a number c: ");
+int c = int.Parse(Console.ReadLine());
+
+int max=a;
+if (b > max)
+max=b;
+if (c > max)
+max=c;
+
+Console.Write("max=");
+Console.WriteLine(max);
