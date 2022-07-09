dff2d54b58420c66a769464cbfed874cf76001fd
Marinapanasyk <marinapanasyk72@mail.ru>
2022-07-09 21:43:16 +0300

    задание_1_2

--- Homework/Lesson_1/1.1/Program.cs
+++ Homework/Lesson_1/1.1/Program.cs
@@ -1,10 +1,17 @@
-﻿// Напишите программу, которая на вход принимает число 
-// и выдает его квадрат (число умноженное на само себя);
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 
-Console.WriteLine ("Write a number:");
+Console.WriteLine ("Write a numberа:");
+Console.WriteLine ("Write b numberb:");
 
 int a = int.Parse(Console.ReadLine());
-Console.WriteLine(a * a);
+int b = int.Parse(Console.ReadLine());
+Console.Write("max=");
+if a > b;
+Console.WriteLine(a);
+else
+{Console.WriteLine(b);
+    
+}
 
 
 
