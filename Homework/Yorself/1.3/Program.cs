19335cb369ba654c1beba4f89ec99f27da6805ed
Marinapanasyk <marinapanasyk72@mail.ru>
2022-07-10 21:57:49 +0300

    самостоятельные6

---
+++ Homework/Yorself/1.3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Особое приветсвие.
+Console.WriteLine ("Введите ваше имя:");
+
+string username = Console.ReadLine ();
+
+if (username.ToLower() == "маша")
+
+    Console.WriteLine("Урра, это же Маша!");
+
+else
+
+Console.WriteLine("Привет");
+
