beb5ed95b3c6195d596a73fc4b8d5f7dfa96a1ae
Marinapanasyk <marinapanasyk72@mail.ru>
2022-07-13 09:37:48 +0300

    Урок 1.3.

--- Homework/Lesson_2/Homework2/1.3/Program.cs
+++ Homework/Lesson_2/Homework2/1.3/Program.cs
@@ -1,4 +1,5 @@
-﻿void Method2(int N)
+﻿//Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+void Method2(int N)
 {
     Console.WriteLine(N);
     Console.Write(N / 100);
