b57039f2a7ad948e753cd17bcb015cbc6fa7e049
Marinapanasyk <marinapanasyk72@mail.ru>
2022-07-15 11:46:29 +0300

    ДЗ_2

---
+++ Homework/Lesson_3/Homework3/1.2/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве..
+
+double Distance (double x1, double y1, double Z1, double x2, double y2, double Z2)
+{
+    return Math.Round(Math.Sqrt(Math.Pow(x1 - x2,2) + Math.Pow(y1 - y2,2)+ Math.Pow(Z1 - Z2,2)),2);
+}
+
+Console.WriteLine (Distance(3, 6, 8, 2, 1, -7));
