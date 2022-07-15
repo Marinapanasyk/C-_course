beffd077bf54786f16ef5aea4b3a1a7e70515a3b
Marinapanasyk <marinapanasyk72@mail.ru>
2022-07-15 12:48:07 +0300

    ДЗ_1

---
+++ Homework/Lesson_3/Homework3/1.1/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+void polindrome (string s )
+{
+    for (int i = 0; i < s.Length / 2; ++i)
+        if (s[i] != s[s.Length - 1 - i]) Console.WriteLine ("не палиндром");
+        else Console.WriteLine ("палиндром");
+};
+var v = 14212;
+var s = v.ToString();
+polindrome (s);
