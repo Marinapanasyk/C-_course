398aa2a2ed822e8f3250b997c947609733d988ac
Marinapanasyk <marinapanasyk72@mail.ru>
2022-08-02 10:54:29 +0300

    урок_2

---
+++ Homework/Lesson_4/1.3/Program.cs
@@ -0,0 +1,10 @@
+﻿//Напишите программу, которая 
+//принимает на вход число N и выдаёт 
+//произведение чисел от 1 до N.
+
+void Length (int a)
+{
+    string num = a.ToString();
+    Console.WriteLine (num.Length);
+}
+Length(167);
