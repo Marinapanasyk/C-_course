5ea3ca5c96324d1e6237b663be695daeb7fa2706
Marinapanasyk <marinapanasyk72@mail.ru>
2022-08-03 13:16:40 +0300

    ДЗ_2

---
+++ Homework/Lesson_4/Homework/1.2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе
+void Method1(int a)
+{
+    int Sum = 0;
+
+    while (a > 0)
+    {
+        Sum = Sum + a % 10;
+        a = a / 10;
+    }
+
+    Console.WriteLine(Sum);
+}
+Method1(456);
