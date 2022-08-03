099ee25df2f1d64305e55115dc9be599cfc06b19
Marinapanasyk <marinapanasyk72@mail.ru>
2022-08-03 15:27:53 +0300

    ДЗ_3

---
+++ Homework/Lesson_4/Homework/1.3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран
+
+int[] Array(int num)
+{
+    int[] array1 = new int[num];
+    for(int i=0; i < num; i++)
+        array1[i]=new Random().Next(0, 10);
+    return array1;
+}
+
+void Arrayprint(int[] array2)
+{
+    for(int i=0; i < array2.Length; i++)
+        Console.WriteLine($"{array2[i]} ");
+}
+Arrayprint(Array(8));
