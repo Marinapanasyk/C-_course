2074fa8c9f3eb670364f7869aa5e1ed4b7ed8152
Marinapanasyk <marinapanasyk72@mail.ru>
2022-08-15 17:38:20 +0300

    Homework7.1

---
+++ Homework/Lesson_7/Homework_7/1.1/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами
+    double[,] fillArray()
+{
+   Console.WriteLine("Введите количество строк m=");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов n=");
+int n = int.Parse(Console.ReadLine());
+double[,] array = new double[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            array[i, j] = Math.Round(new Random().NextDouble() * (100 + 150) - 100, 2); ;
+        }
+    }
+    return array;
+}
+
+void printArray(double[,] array)
+{
+    for (int i = 0; i <  array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} "); 
+        }
+        Console.WriteLine();
+    }
+}
+
+printArray(fillArray());
