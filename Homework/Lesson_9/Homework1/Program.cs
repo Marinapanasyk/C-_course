ddc26a3e2bd3ae51cf0ba449755962980594dcba
Marinapanasyk <marinapanasyk72@mail.ru>
2022-08-19 13:37:34 +0300

    Homework9.1.1

---
+++ Homework/Lesson_9/Homework1/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задайте значения M и N. Напишите программу, которая выведет все чётные натуральные числа в промежутке от M до N.
+
+
+void ValueSetMinMax(int min, int max)
+{
+    if (max<min) return;
+    ValueSetMinMax(min,max-1);
+    if (max % 2 == 0)  
+    Console.Write($"{max}, ");}
+
+Console.Write("Введите M: ");
+int min = int.Parse(Console.ReadLine());
+Console.Write("Введите N: ");
+int max = int.Parse(Console.ReadLine());
+ValueSetMinMax(min,max);
